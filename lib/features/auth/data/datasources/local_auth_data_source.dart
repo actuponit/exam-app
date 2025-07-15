8ddd1624d88e5389fd62deb8c84ab1ff971ccc41
Imran Mohammed <imo60067035@gmail.com>
2025-07-15 16:22:17 +0300

    refactor: Restore user ID retrieval in LocalAuthDataSource
    - Reverted the getUserId method in LocalAuthDataSource to retrieve the user ID from preferences, enhancing functionality.
    - Commented out the hardcoded return value for clarity and to prevent confusion in future development.

--- lib/features/auth/data/datasources/local_auth_data_source.dart
+++ lib/features/auth/data/datasources/local_auth_data_source.dart
@@ -29,8 +29,8 @@ class LocalAuthDataSourceImpl implements LocalAuthDataSource {
 
   @override
   Future<int?> getUserId() async {
-    // return _prefs.getInt(_userIdKey);
-    return 170;
+    return _prefs.getInt(_userIdKey);
+    // return 170;
   }
 
   @override
